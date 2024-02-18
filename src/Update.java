34cd9036cc3465bda502f222c322bc0372cd09c5
Kayle Dee <kdaniels24@basecampcodingacademy.org>
2024-02-18 17:49:49 -0600

    commmit

---
+++ src/Update.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql,DriverManger;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UpdateStatus {
+    public static void main(String[] args) {
+        String sql = "...";
+        String name = "username";
+        String pass = "password";
+
+        try (Connection connection =)
+    }
+}
